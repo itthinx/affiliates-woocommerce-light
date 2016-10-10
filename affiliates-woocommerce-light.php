08921e738c2dec261ce0835946f2f73532b977d6
itthinx <itthinx@gmail.com>
2016-10-10 14:08:16 +0200

    current version 1.3.0 from svn

---
+++ affiliates-woocommerce-light.php
@@ -0,0 +1,345 @@
+<?php
+/**
+ * affiliates-woocommerce-light.php
+ * 
+ * Copyright (c) 2012-2015 "kento" Karim Rahimpur www.itthinx.com
+ * 
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * This header and all notices must be kept intact.
+ * 
+ * @author Karim Rahimpur
+ * @package affiliates-woocommerce-light
+ * @since affiliates-woocommerce-light 1.0.0
+ *
+ * Plugin Name: Affiliates WooCommerce Integration Light
+ * Plugin URI: http://www.itthinx.com/plugins/affiliates-woocommerce-light/
+ * Description: Integrates Affiliates with WooCommerce
+ * Version: 1.3.0
+ * Author: itthinx
+ * Author URI: http://www.itthinx.com/
+ * Donate-Link: http://www.itthinx.com/shop/affiliates-enterprise/
+ * License: GPLv3
+ */
+
+/**
+ * Light integration for WooCommerce.
+ */
+class Affiliates_WooCommerce_Light_Integration {
+
+	const SHOP_ORDER_POST_TYPE = 'shop_order';
+	const PLUGIN_OPTIONS = 'affiliates_woocommerce_light';
+	const AUTO_ADJUST_DEFAULT = true;
+	const NONCE = 'aff_woo_light_admin_nonce';
+	const SET_ADMIN_OPTIONS = 'set_admin_options';
+	const REFERRAL_RATE = "referral-rate";
+	const REFERRAL_RATE_DEFAULT = "0";
+	
+	const USAGE_STATS = 'usage_stats';
+	const USAGE_STATS_DEFAULT = true;
+
+	/**
+	 * Links to posts of type shop_order will be modified only on these
+	 * admin pages.
+	 * 
+	 * @var array
+	 */
+	private static $shop_order_link_modify_pages = array(
+		'affiliates-admin-referrals',
+		'affiliates-admin-hits',
+		'affiliates-admin-hits-affiliate'
+	);
+
+	private static $admin_messages = array();
+
+	/**
+	 * Prints admin notices.
+	 */
+	public static function admin_notices() {
+		if ( !empty( self::$admin_messages ) ) {
+			foreach ( self::$admin_messages as $msg ) {
+				echo $msg;
+			}
+		}
+	}
+
+	/**
+	 * Checks dependencies and adds appropriate actions and filters.
+	 */
+	public static function init() {
+
+		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
+
+		$verified = true;
+		$disable = false;
+		$active_plugins = get_option( 'active_plugins', array() );
+		if ( is_multisite() ) {
+			$active_sitewide_plugins = get_site_option( 'active_sitewide_plugins', array() );
+			$active_sitewide_plugins = array_keys( $active_sitewide_plugins );
+			$active_plugins = array_merge( $active_plugins, $active_sitewide_plugins );
+		}
+		$affiliates_is_active = in_array( 'affiliates/affiliates.php', $active_plugins ) || in_array( 'affiliates-pro/affiliates-pro.php', $active_plugins ) || in_array( 'affiliates-enterprise/affiliates-enterprise.php', $active_plugins );
+		$woocommerce_is_active = in_array( 'woocommerce/woocommerce.php', $active_plugins );
+		$affiliates_woocommerce_is_active = in_array( 'affiliates-woocommerce/affiliates-woocommerce.php', $active_plugins );
+		if ( !$affiliates_is_active ) {
+			self::$admin_messages[] = "<div class='error'>" . __( 'The <strong>Affiliates WooCommerce Integration Light</strong> plugin requires the <a href="http://wordpress.org/plugins/affiliates/">Affiliates</a> plugin.', 'affiliates-woocommerce-light' ) . "</div>";
+		}
+		if ( !$woocommerce_is_active ) {
+			self::$admin_messages[] = "<div class='error'>" . __( 'The <strong>Affiliates WooCommerce Integration Light</strong> plugin requires the <a href="http://wordpress.org/plugins/woocommerce/">WooCommerce</a> plugin to be activated.', 'affiliates-woocommerce-light' ) . "</div>";
+		}
+		if ( $affiliates_woocommerce_is_active ) {
+			self::$admin_messages[] = "<div class='error'>" . __( 'You do not need to use the <srtrong>Affiliates WooCommerce Integration Light</strong> plugin because you are already using the advanced Affiliates WooCommerce Integration plugin. Please deactivate the <strong>Affiliates WooCommerce Integration Light</strong> plugin now.', 'affiliates-woocommerce-light' ) . "</div>";
+		}
+		if ( !$affiliates_is_active || !$woocommerce_is_active || $affiliates_woocommerce_is_active ) {
+			if ( $disable ) {
+				include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+				deactivate_plugins( array( __FILE__ ) );
+			}
+			$verified = false;
+		}
+
+		if ( $verified ) {
+			add_action ( 'woocommerce_checkout_order_processed', array( __CLASS__, 'woocommerce_checkout_order_processed' ) );
+			$options = get_option( self::PLUGIN_OPTIONS , array() );
+			add_filter( 'post_type_link', array( __CLASS__, 'post_type_link' ), 10, 4 );
+			add_action( 'affiliates_admin_menu', array( __CLASS__, 'affiliates_admin_menu' ) );
+			add_filter( 'affiliates_footer', array( __CLASS__, 'affiliates_footer' ) );
+			add_filter( 'affiliates_setup_buttons', array( __CLASS__, 'affiliates_setup_buttons' ) );
+		}
+	}
+
+	/**
+	 * Add a setup hint button.
+	 *
+	 * @param array $buttons
+	 * @return array
+	 */
+	public static function affiliates_setup_buttons( $buttons ) {
+		$buttons['affiliates-woocommerce-light'] = sprintf (
+			'<a href="%s" class="button-primary">%s</a>',
+			add_query_arg( 'section', 'affiliates-woocommerce-light', admin_url( 'admin.php?page=affiliates-admin-woocommerce-light' ) ),
+			__( 'Set the Commission Rate', AFFILIATES_PLUGIN_DOMAIN )
+		);
+		return $buttons;
+	}
+
+	/**
+	 * Adds a submenu item to the Affiliates menu for the WooCommerce integration options.
+	 */
+	public static function affiliates_admin_menu() {
+		$page = add_submenu_page(
+			'affiliates-admin',
+			__( 'Affiliates WooCommerce Integration Light', 'affiliates-woocommerce-light' ),
+			__( 'WooCommerce Integration Light', 'affiliates-woocommerce-light' ),
+			AFFILIATES_ADMINISTER_OPTIONS,
+			'affiliates-admin-woocommerce-light',
+			array( __CLASS__, 'affiliates_admin_woocommerce_light' )
+		);
+		$pages[] = $page;
+		add_action( 'admin_print_styles-' . $page, 'affiliates_admin_print_styles' );
+		add_action( 'admin_print_scripts-' . $page, 'affiliates_admin_print_scripts' );
+	}
+
+	/**
+	 * Affiliates WooCommerce Integration Light : admin section.
+	 */
+	public static function affiliates_admin_woocommerce_light() {
+		$output = '';
+		if ( !current_user_can( AFFILIATES_ADMINISTER_OPTIONS ) ) {
+			wp_die( __( 'Access denied.', 'affiliates-woocommerce-light' ) );
+		}
+		$options = get_option( self::PLUGIN_OPTIONS , array() );
+		if ( isset( $_POST['submit'] ) ) {
+			if ( wp_verify_nonce( $_POST[self::NONCE], self::SET_ADMIN_OPTIONS ) ) {
+				$options[self::REFERRAL_RATE]  = floatval( $_POST[self::REFERRAL_RATE] );
+				if ( $options[self::REFERRAL_RATE] > 1.0 ) {
+					$options[self::REFERRAL_RATE] = 1.0;
+				} else if ( $options[self::REFERRAL_RATE] < 0 ) {
+					$options[self::REFERRAL_RATE] = 0.0;
+				}
+				$options[self::USAGE_STATS] = !empty( $_POST[self::USAGE_STATS] );
+			}
+			update_option( self::PLUGIN_OPTIONS, $options );
+		}
+
+		$referral_rate = isset( $options[self::REFERRAL_RATE] ) ? $options[self::REFERRAL_RATE] : self::REFERRAL_RATE_DEFAULT; 
+		$usage_stats   = isset( $options[self::USAGE_STATS] ) ? $options[self::USAGE_STATS] : self::USAGE_STATS_DEFAULT;
+
+		$output .=
+			'<div>' .
+			'<h2>' .
+			__( 'Affiliates WooCommerce Integration Light', 'affiliates-woocommerce-light' ) .
+			'</h2>' .
+			'</div>';
+
+		$output .= '<p class="manage" style="padding:1em;margin-right:1em;font-weight:bold;font-size:1em;line-height:1.62em">';
+		$output .= __( 'You can support the development of the Affiliates plugin and get additional features with <a href="http://www.itthinx.com/shop/affiliates-pro/" target="_blank">Affiliates Pro</a> and <a href="http://www.itthinx.com/shop/affiliates-enterprise/" target="_blank">Affiliates Enterprise</a>.', 'affiliates-woocommerce-light' );
+		$output .= '</p>';
+
+		$output .= '<div class="manage" style="padding:2em;margin-right:1em;">';
+		$output .= '<form action="" name="options" method="post">';        
+		$output .= '<div>';
+		$output .= '<h3>' . __( 'Referral Rate', 'affiliates-woocommerce-light' ) . '</h3>';
+		$output .= '<p>';
+		$output .= '<label for="' . self::REFERRAL_RATE . '">' . __( 'Referral rate', 'affiliates-woocommerce-light') . '</label>';
+		$output .= '&nbsp;';
+		$output .= '<input name="' . self::REFERRAL_RATE . '" type="text" value="' . esc_attr( $referral_rate ) . '"/>';
+		$output .= '</p>';
+		$output .= '<p>';
+		$output .= __( 'The referral rate determines the referral amount based on the net sale made.', 'affiliates-woocommerce-light' );
+		$output .= '</p>';
+		$output .= '<p class="description">';
+		$output .= __( 'Example: Set the referral rate to <strong>0.1</strong> if you want your affiliates to get a <strong>10%</strong> commission on each sale.', 'affiliates-woocommerce-light' );
+		$output .= '</p>';
+
+		$output .= '<h3>' . __( 'Usage stats', 'affiliates-woocommerce-light' ) . '</h3>';
+		$output .= '<p>';
+		$output .= '<input name="' . self::USAGE_STATS . '" type="checkbox" ' . ( $usage_stats ? ' checked="checked" ' : '' ) . '/>';
+		$output .= ' ';
+		$output .= '<label for="' . self::USAGE_STATS . '">' . __( 'Allow the plugin to provide usage stats.', 'affiliates-woocommerce-light' ) . '</label>';
+		$output .= '<br/>';
+		$output .= '<span class="description">' . __( 'This will allow the plugin to help in computing how many installations are actually using it. No personal or site data is transmitted, this simply embeds an icon on the bottom of the Affiliates admin pages, so that the number of visits to these can be counted. This is useful to help prioritize development.', 'affiliates-woocommerce-light' ) . '</span>';
+		$output .= '</p>';
+
+		$output .= '<p>';
+		$output .= wp_nonce_field( self::SET_ADMIN_OPTIONS, self::NONCE, true, false );
+		$output .= '<input class="button-primary" type="submit" name="submit" value="' . __( 'Save', 'affiliates-woocommerce-light' ) . '"/>';
+		$output .= '</p>';
+
+		$output .= '</div>';
+		$output .= '</form>';
+		$output .= '</div>';
+
+		echo $output;
+
+		affiliates_footer();
+	}
+	
+	/**
+	 * Add a notice to the footer that the integration is active.
+	 * @param string $footer
+	 */
+	public static function affiliates_footer( $footer ) {
+		$options = get_option( self::PLUGIN_OPTIONS , array() );
+		$usage_stats   = isset( $options[self::USAGE_STATS] ) ? $options[self::USAGE_STATS] : self::USAGE_STATS_DEFAULT;
+		return
+			'<div style="font-size:0.9em">' .
+			'<p>' .
+			( $usage_stats ? "<img src='http://www.itthinx.com/img/affiliates-woocommerce/affiliates-woocommerce-light.png' alt='Logo'/>" : '' ) .
+			__( "Powered by <a href='http://www.itthinx.com/plugins/affiliates-woocommerce-light' target='_blank'>Affiliates WooCommerce Integration Light</a>.", 'affiliates-woocommerce-light' ) .
+			'</p>' .
+			'</div>' .
+			$footer;
+	}
+
+	/**
+	 * Returns an edit link for shop_order post types.
+	 * 
+	 * @param string $post_link
+	 * @param array $post
+	 * @param boolean $leavename
+	 * @param boolean $sample
+	 */
+	public static function post_type_link( $post_link, $post, $leavename, $sample ) {
+		$link = $post_link;
+		if (
+			// right post type
+			isset( $post->post_type) && ( $post->post_type == self::SHOP_ORDER_POST_TYPE ) &&
+			// admin page
+			is_admin() &&
+			// right admin page
+			isset( $_REQUEST['page'] ) && in_array( $_REQUEST['page'], self::$shop_order_link_modify_pages ) &&
+			// check link 
+			(
+				( preg_match( "/" . self::SHOP_ORDER_POST_TYPE . "=([^&]*)/", $post_link, $matches ) === 1 ) && isset( $matches[1] ) && ( $matches[1] === $post->post_name )
+				||
+				( strpos( $post_link, 'post_type=' . self::SHOP_ORDER_POST_TYPE ) !== false ) && ( preg_match( '/p=([0-9]+)/', $post_link, $matches ) === 1 ) && isset( $matches[1] ) && ( $matches[1] == $post->ID )
+			)
+		) {
+			$link = admin_url( 'post.php?post=' . $post->ID . '&action=edit' );
+		}
+		return $link;
+	}
+
+	/**
+	 * Record a referral when a new order has been processed.
+	 * 
+	 * Note that we can't hook into the order process before(*), because
+	 * the meta data would not have been added.
+	 * 
+	 * (*) We could hook into woocommerce_checkout_update_order_meta but the
+	 * 'coupons' meta data would not be there, so if we want to use it here at
+	 * some point, woocommerce_checkout_order_processed is a better choice.
+	 * @param int $order_id the post id of the order
+	 */
+	public static function woocommerce_checkout_order_processed( $order_id ) {
+
+		$order_subtotal = null;
+		$currency       = get_option( 'woocommerce_currency' );
+
+		if ( function_exists( 'wc_get_order' ) ) {
+			if ( $order = wc_get_order( $order_id ) ) {
+				if ( method_exists( $order, 'get_subtotal' ) ) {
+					$order_subtotal = $order->get_subtotal();
+				}
+				if ( method_exists( $order, 'get_total_discount' ) ) {
+					$order_subtotal -= $order->get_total_discount(); // excluding tax
+					if ( $order_subtotal < 0 ) {
+						$order_subtotal = 0;
+					}
+				}
+				if ( method_exists( $order, 'get_order_currency' ) ) {
+					$currency = $order->get_order_currency();
+				}
+			}
+		}
+
+		if ( $order_subtotal === null ) {
+			$order_total        = get_post_meta( $order_id, '_order_total', true );
+			$order_tax          = get_post_meta( $order_id, '_order_tax', true );
+			$order_shipping     = get_post_meta( $order_id, '_order_shipping', true );
+			$order_shipping_tax = get_post_meta( $order_id, '_order_shipping_tax', true );
+			$order_subtotal     = $order_total - $order_tax - $order_shipping - $order_shipping_tax;
+		}
+
+		$order_link = '<a href="' . admin_url( 'post.php?post=' . $order_id . '&action=edit' ) . '">';
+		$order_link .= sprintf( __( 'Order #%s', 'affiliates-woocommerce-light' ), $order_id );
+		$order_link .= "</a>";
+
+		$data = array(
+			'order_id' => array(
+				'title' => 'Order #',
+				'domain' => 'affiliates-woocommerce-light',
+				'value' => esc_sql( $order_id )
+			),
+			'order_total' => array(
+				'title' => 'Total',
+				'domain' =>  'affiliates-woocommerce-light',
+				'value' => esc_sql( $order_subtotal )
+			),
+			'order_currency' => array(
+				'title' => 'Currency',
+				'domain' =>  'affiliates-woocommerce-light',
+				'value' => esc_sql( $currency )
+			),
+			'order_link' => array(
+				'title' => 'Order',
+				'domain' =>  'affiliates-woocommerce-light',
+				'value' => esc_sql( $order_link )
+			)
+		);
+
+		$options = get_option( self::PLUGIN_OPTIONS , array() );
+		$referral_rate  = isset( $options[self::REFERRAL_RATE] ) ? $options[self::REFERRAL_RATE] : self::REFERRAL_RATE_DEFAULT;
+		$amount = round( floatval( $referral_rate ) * floatval( $order_subtotal ), AFFILIATES_REFERRAL_AMOUNT_DECIMALS );
+		$description = sprintf( 'Order #%s', $order_id );
+		affiliates_suggest_referral( $order_id, $description, $data, $amount, $currency );
+	}
+}
+Affiliates_WooCommerce_Light_Integration::init();
---
+++ readme.txt
@@ -0,0 +1,128 @@
+=== Affiliates WooCommerce Integration Light ===
+Contributors: itthinx
+Donate link: http://www.itthinx.com/plugins/affiliates-woocommerce-light
+Tags: ads, advertising, affiliate, affiliate marketing, affiliate plugin, affiliate tool, affiliates, bucks, contact form, crm, earn money, e-commerce, e-commerce, integration, lead, link, marketing, money, online sale, order, partner, referral, referral links, referrer, shopping cart, sales, shop, shopping cart, site, track, transaction, WooCommerce, WordPress
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: 1.3.0
+License: GPLv3
+
+This plugin integrates Affiliates with WooCommerce.
+
+== Description ==
+
+This plugin integrates [Affiliates](http://wordpress.org/plugins/affiliates/) with [WooCommerce](http://wordpress.org/plugins/woocommerce/).
+
+With this integration plugin, referrals are created automatically for your affiliates when sales are made.
+
+The plugin allows you to set a referral (commission) rate so that your affiliates get credited with a referral based on a percentage of each sale's total net amount.
+
+An advanced integration that synchronizes the referrals to their order status is available with [Affiliates Pro](http://www.itthinx.com/shop/affiliates-pro/) and [Affiliates Enterprise](http://www.itthinx.com/shop/affiliates-enterprise/).
+
+*Affiliates* is an outstanding platform that helps to boost sales through your affiliate program and is the ideal affiliate solution for *WooCommerce*.
+
+Requirements:
+
+- [Affiliates](http://wordpress.org/extend/plugins/affiliates)
+- [Affiliates WooCommerce Integration Light](http://wordpress.org/extend/plugins/affiliates-woocommerce-light) (this plugin)
+- [WooCommerce](http://wordpress.org/extend/plugins/woocommerce)
+
+Install these, set up your shop, decide how much you want to pay your affiliates and start selling!
+
+__Feedback__ is welcome!
+If you need help, have problems, want to leave feedback or want to provide constructive criticism, you can leave a comment here at the [plugin page](http://www.itthinx.com/plugins/affiliates-woocommerce-light).
+
+Please try to solve problems there before you rate this plugin or say it doesn't work. There goes a _lot_ of work into providing you with free quality plugins! Please appreciate that and help with your feedback. Thanks!
+
+You are welcome to [follow itthinx](http://twitter.com/itthinx) on Twitter for updates on this and related plugins.
+
+== Installation ==
+
+1. Install and activate the [WooCommerce](http://wordpress.org/extend/plugins/woocommerce) plugin. Configure your shop and create products.
+2. Install and activate the [Affiliates](http://wordpress.org/extend/plugins/affiliates) plugin. Use the default settings or configure it to your needs.
+3. Install and activate the [Affiliates WooCommerce Integration Light](http://wordpress.org/extend/plugins/affiliates-woocommerce-light) plugin.
+4. A new *WooCommerce Integration Light* menu item will appear under the *Affiliates* menu in WordPress. Set the referral rate for your affiliates there.
+
+Note that you can install the plugins from your WordPress installation directly: use the *Add new* option found in the *Plugins* menu.
+You can also upload and extract them in your site's `/wp-content/plugins/` directory or use the *Upload* option.
+
+== Frequently Asked Questions ==
+
+= What features does this integration provide? =
+
+When a sale is made through WooCommerce, a referral is recorded for the affiliate that referred the sale.
+
+= How can I set the amount that affiliates earn on each sale? =
+
+Go to *Affiliates > WooCommerce Integration Light* and set the rate there.
+
+Example: If you want to give an affiliate 10% of each net total sales amount, set the rate to *0.1*.
+
+== Screenshots ==
+
+See also: [Affiliates WooCommerce Light](http://www.itthinx.com/plugins/affiliates-woocommerce-light/)
+
+1. Referral rate setting - Here, a referral rate set at 0.25 credits affiliates with 25% commissions on sales.
+2. Referrals created automatically for sales - Each referral is linked to its order.
+
+== Changelog ==
+
+= 1.3.0 =
+* WordPress 4.5 compatible.
+* Replaced usage of constant with string literal for the plugin's language domain.
+
+= 1.2.0 =
+* Updated the commission calculation to take discounts excluding tax into account. 
+
+= 1.1.0 =
+* Updated the referral calculation to use order methods.
+* Updated the plugin version info, tested for WordPress 4.2 and requires at least WordPress 4.0.
+* Fixed the path to WooCommerce on wordpress.org.
+* Updated the plugin info and added information on features and advanced versions.
+
+= 1.0.12 =
+* Added a setup hint button to set the commission rate.
+
+= 1.0.11 =
+* WordPress 4.1 compatibility checked.
+
+= 1.0.10 =
+* Added button class in admin section.
+* Tested for WordPress 4.0 and WooCommerce 2.2.0 compatibility.
+
+= 1.0.9 =
+* Settings page cleaned up, reduced note footprint.
+
+= 1.0.8 =
+* WordPress 3.9 compatibility checked.
+
+= 1.0.7 =
+* WordPress 3.8 compatibility checked.
+
+= 1.0.6 =
+* WP 3.6 compatibility tested
+
+= 1.0.5 =
+* Fixed links to orders with ugly permalink settings
+
+= 1.0.4 =
+* WP 3.5 compatibility tested
+
+= 1.0.3 =
+* WP 3.4.1 compatibility
+
+= 1.0.2 =
+* Fixed links to orders on _Visits & Referrals_ and _Affiliates & Referrals_ admin screens
+* Added usage stats option
+* Reduced footprint on affiliates admin pages 
+
+= 1.0.1 =
+* Referral amount calculation adjusted to missing subtotal (WooCommerce > 1.3.2.1)
+
+= 1.0.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.3.0 =
+* This release has been tested with WordPress 4.5 and provides an internal improvement for translations.
